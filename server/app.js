36404438589c2b4859f019ea74424c959b685b4f
Moritz Salla <moritz@buildinamsterdam.com>
2023-02-03 22:27:33 +0100

    🧹 Cleanup `server` dir

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const path = require('path');
-
 const index = require('./index');
 
+const PORT = process.env.PORT || 4001;
+
 const app = express();
+
 app.use(index);
-// app.use(express.static(path.join(__dirname, 'client/build')));
 
 const server = http.createServer(app);
 const io = socketIo(server);
@@ -19,19 +19,18 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('- Client disconnected');
+
     getApiAndEmit(socket);
   });
 });
 
 const getApiAndEmit = (socket) => {
   console.log('emit event');
+
   io.emit('event', io.engine.clientsCount);
   socket.broadcast.emit('event', io.engine.clientsCount);
 };
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/client/build/index.html'));
-// });
-
-const port = process.env.PORT || 4001;
-server.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
